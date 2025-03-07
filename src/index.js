fcdeda69d35a52ec4e7e6f906806ecd790bd8ce5
smmac72 <smmac072@gmail.com>
2025-03-08 02:32:22 +0300

    analytics transfer to index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import './styles/global.css';
 import analytics from './utils/analytics';
 
 const YANDEX_METRIKA_ID = '100250172'; // should i be afraid
+
+const loadYandexMetrika = () => {
+  const script = document.createElement('script');
+  script.src = 'https://mc.yandex.ru/metrika/tag.js';
+  script.async = true;
+  script.crossOrigin = 'anonymous';
+  script.setAttribute('data-cookies-policy', 'crosssite');
+  document.head.appendChild(script);
+};
+window.ym(YANDEX_METRIKA_ID, "init", {
+  clickmap: true,
+  trackLinks: true,
+  accurateTrackBounce: true,
+  webvisor: true,
+  ecommerce: "dataLayer",
+  defer: true,
+  cookieFlags: "crossDomain" 
+});
+
+loadYandexMetrika();
 analytics.init(YANDEX_METRIKA_ID);
 analytics.trackEvent('System', 'PageLoad', window.location.href);
 
