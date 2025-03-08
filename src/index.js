e10b54f73f48f4af1371bf9b13523f52c8f9d716
zeromac <smmac072@gmail.com>
2025-03-08 08:41:10 +0300

    some wizardy stuff

--- src/index.js
+++ src/index.js
@@ -7,33 +7,33 @@ import analytics from './utils/analytics';
 const YANDEX_METRIKA_ID = '100250172';
 
 const initYandexMetrika = () => {
-  window.ym = window.ym || function() {
-    (window.ym.a = window.ym.a || []).push(arguments);
-  };
-  window.ym.l = 1 * new Date();
-  
-  const script = document.createElement("script");
-  script.async = 1;
-  script.src = "https://mc.yandex.ru/metrika/tag.js";
-  
-  const firstScript = document.getElementsByTagName("script")[0];
-  firstScript.parentNode.insertBefore(script, firstScript);
+  (function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+  })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
   
   window.ym(YANDEX_METRIKA_ID, "init", {
     clickmap: true,
     trackLinks: true,
     accurateTrackBounce: true,
     webvisor: true,
-    ecommerce: "dataLayer",
+    trackHash: true,
+    ut: 'noindex',
     defer: true,
-    crossDomain: true
+    accurateTopicsInference: false,
+    disableTopics: true,
+    cookieFlags: 'domain=auto;secure;samesite=none'
   });
 };
 
-initYandexMetrika();
-analytics.init(YANDEX_METRIKA_ID);
-analytics.trackEvent('System', 'PageLoad', window.location.href);
+document.addEventListener('DOMContentLoaded', () => {
+  initYandexMetrika();
+  analytics.init(YANDEX_METRIKA_ID);
+  analytics.trackEvent('System', 'PageLoad', window.location.href);
+});
 
+// Render the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
